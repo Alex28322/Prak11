d39fc86de726ad647f237b30808417fa232650c1
Alex28322 <sander2832@gmail.com>
2024-12-02 17:10:08 +0200

    3 page

--- js/script.js
+++ js/script.js
@@ -1,6 +1,33 @@
 document.querySelector('.banner-blue__cross').addEventListener('click', function() {
     document.querySelector('.banner-blue').classList.add('active');
 });
+document.querySelectorAll(".productdata__item").forEach(function (item) {
+    item.addEventListener("click", function () {
+        item.classList.toggle("active");
+        document.querySelector(".productdata__arrow-mobile").classList.toggle("active");
+        
+    });
+});
+
+let position = 0;
+const line = document.querySelector(".discount__scroll");
+const scrollwidth = line.scrollWidth
+
+document.querySelector('.discount__button-right').addEventListener('click', function(){
+    position = position + scrollwidth / 4;
+    if (position > (3 * scrollwidth) / 4){
+        position = 0
+    }
+    line.style.left = - position + 'px'
+});
+document.querySelector('.discount__button-left').addEventListener('click', function(){
+    position = position - scrollwidth / 4;
+    if (position < 0){
+        position = (3 * scrollwidth) / 4
+    }
+    line.style.left = - position + 'px'
+});
+
 const textBox = document.querySelector('.intro__text-box');
 const hideButton = document.querySelector('.hide');
 const openButton = document.querySelector('.open');
